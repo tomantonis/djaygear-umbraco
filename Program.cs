306adfc41534b0ffbfecaba4cf8e1f6dd65cb072
Tom Antonis <tom@antonis.nl>
2025-12-07 12:40:09 +0100

    Upgrade 17.0.1

--- Program.cs
+++ Program.cs
@@ -1,36 +1,24 @@
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
 builder.CreateUmbracoBuilder()
-    .AddBackOffice()
-    .AddWebsite()
-    .AddDeliveryApi()
-    .AddComposers()
-    .Build();
+       .AddBackOffice()
+       .AddWebsite()
+       .AddDeliveryApi()
+       .AddComposers()
+       .Build();
 
-WebApplication app = builder.Build();
+var app = builder.Build();
 
 await app.BootUmbracoAsync();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseDeveloperExceptionPage();
-}
-else
-{
-    app.UseHttpsRedirection();
-    app.UseHsts();
-}
-
 app.UseUmbraco()
-    .WithMiddleware(u =>
-    {
-        u.UseBackOffice();
-        u.UseWebsite();
-    })
-    .WithEndpoints(u =>
-    {
-        u.UseBackOfficeEndpoints();
-        u.UseWebsiteEndpoints();
-    });
+   .WithMiddleware(u => {
+       u.UseBackOffice();
+       u.UseWebsite();
+   })
+   .WithEndpoints(u => {
+       u.UseBackOfficeEndpoints();
+       u.UseWebsiteEndpoints();
+   });
 
 await app.RunAsync();
--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # djaygear-umbraco
+
+## Upgrade
+Replace version with new version:
+`dotnet add package Umbraco.Cms --version 17.0.0`
+
+## Restore
+`dotnet restore`
+
+## Build
+`dotnet build`
+
+## Publish
+`dotnet publish -o ../build -f net10.0`
